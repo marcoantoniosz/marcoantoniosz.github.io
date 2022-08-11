e15b271793125e0f5042135d98d2621067c9c324
Marco <marcoantonio99d@gmail.com>
2022-08-11 11:23:14 -0300

    Projects Slide

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -2,11 +2,11 @@ import React, { useContext } from 'react';
 import MyContext from '../../context/MyContext';
 import MenuMobile from '../../components/menu';
 import Header from '../../components/header';
-import Card from '../../components/card'
 import Footer from '../../components/footer';
 import projectsMage from '../../images/PROJETOS.png';
 import style from './style.module.css';
 import data from '../../data/projectsdata';
+import ProjectsSlide from '../../components/projectslide';
 
 export default function Projects() {
   const { menuOn } = useContext(MyContext);
@@ -16,18 +16,9 @@ export default function Projects() {
         <h1 className={ style.title }>
           Meus projetos
         </h1>
+        <div className={ style.line } />
         <div className={ style.projects }>
-          { data.map(project => (
-            <Card 
-            key={ project.title }
-            title={ project.title }
-            description={ project.description }
-            img={ project.image }
-            techs={ project.techs }
-            stack={ project.stack }
-            link={ project.url }
-             />
-          )) }
+          <ProjectsSlide fadeProjects={ data } />
         </div>
     </main>
   );
