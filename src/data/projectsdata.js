c527148d2cbd7919bfc88910e50070d91a471e36
Marco <marcoantonio99d@gmail.com>
2022-08-10 21:44:31 -0300

    ponto final nas descrições de projeto

--- src/data/projectsdata.js
+++ src/data/projectsdata.js
@@ -7,7 +7,7 @@ const data = [
   {
     title: 'Vehicles Challenge',
     stack: 'Fullstack',
-    description: 'Vehicles Challenge é um web app full stack que permite ao usuário cadastrar, editar e excluir anúncios de veículos',
+    description: 'Vehicles Challenge é um web app full stack que permite ao usuário cadastrar, editar e excluir anúncios de veículos.',
     techs: ['React', 'CSS', 'NodeJS', 'MySQL', 'Express'],
     url: 'https://marcoantoniosz.github.io/vehicles/',
     image: vehicles
@@ -15,7 +15,7 @@ const data = [
   {
     title: 'iTony',
     stack: 'Frontend',
-    description: 'iTony é um web app que permite ao usuário buscar e navegar por álbuns de música, além de salvar suas faixas favoritas',
+    description: 'iTony é um web app que permite ao usuário buscar e navegar por álbuns de música, além de salvar suas faixas favoritas.',
     techs: ['React', 'CSS', 'Hook Form', 'iTunes API'],
     url: 'https://marcoantoniosz.github.io/itony/',
     image: iTony,
@@ -23,7 +23,7 @@ const data = [
   {
     title: 'Pocket Cauldron',
     stack: 'Frontend',
-    description: 'Pocket Cauldron é um web app que permite ao usuário navegar por receitas e drinks, sendo possivel iniciar e finalizar uma receita, incluindo checking de ingredientes',
+    description: 'Pocket Cauldron é um web app que permite ao usuário navegar por receitas e drinks, sendo possivel iniciar e finalizar uma receita, incluindo checking de ingredientes.',
     techs: ['React', 'CSS', 'REDUX', 'TheMealDB API', 'TheCocktailDB API'],
     url: 'https://marcoantoniosz.github.io/pocket-cauldron/',
     image: pocketCaldron
@@ -31,7 +31,7 @@ const data = [
   {
     title: 'Tryvia',
     stack: 'Frontend',
-    description: 'Tryvia é um web app que oferece ao usuário uma partida de trivia em 5 rodadas, sendo possível acessar placar e ranking',
+    description: 'Tryvia é um web app que oferece ao usuário uma partida de trivia em 5 rodadas, sendo possível acessar placar e ranking.',
     techs: ['React', 'CSS', 'REDUX', 'OpenTDB API'],
     url: 'https://marcoantoniosz.github.io/tryvia/',
     image: tryvia
